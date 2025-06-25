9beb0548e5182f1e75c40fcca752e329da438983
Corey Robinson <coreyrob52@gmail.com>
2025-06-25 07:11:46 -0500

    feat: listen and connect to .ovpn files on -p path

--- README.md
+++ README.md
@@ -1,4 +1,47 @@
 # auto-vpn
+Simple program to auto connect to .ovpn files using `openvpn`
 
+<br>
+[Usage](#Usage) <span>&nbsp;•&nbsp;</span> [Install](#Install)
+
+## Usage
+- Run `auto-vpn` with `-p` followed by a file path to listen on
+```bash
+auto-vpn -p ~/Downloads
+```
+- Use `-clean` to remove old .ovpn files before listening
+```bash
+auto-vpn -p ~/Downloads -clean
+```
+
+## Install
+Download pre-built binary for your system here [Releases](https://github.com/CoreyRobinsonDev/auto-vpn/releases).
+
+### Compiling from Source
+- Clone this repository
+```bash
+git clone https://github.com/CoreyRobinsonDev/auto-vpn.git
+```
+- Create **auto-vpn** binary
+```bash
+cd auto-vpn
+go build -o /usr/local/bin
+```
+- Confirm that the program was built successfully
+```bash
+auto-vpn -v
+```
+## Running without Password
+This program is meant to run without intervention in the background. However `openvpn` is ran with `sudo` and `sudo` requires you to enter your user password. <br>
+To get around this do the following:
+
+- Edit `/etc/sudoers` with `visudo`
+```bash
+sudo visudo
+```
+- Add the following with your username and path to `openvpn`
+```bash
+[your_username] ALL=(ALL) NOPASSWD: [/path/to/openvpn]
+```
 ## License
 [The Unlicense](./LICENSE)
--- main.go
+++ main.go
@@ -1,5 +1,115 @@
 package main
 
+import (
+	"flag"
+	"fmt"
+	"os"
+	"os/exec"
+	"os/signal"
+	"runtime"
+	"strings"
+	"syscall"
+	"time"
+)
+
 func main() {
-	println("Hello, World!")
+	pathPtr := flag.String("p", "", "path to listen on")
+	cleanPtr := flag.Bool("clean", false, "remove .ovpn files from path")
+	versionPtr := flag.Bool("v", false, "version")
+	flag.Parse()
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, os.Interrupt, syscall.SIGTERM)
+
+	if *versionPtr {
+		fmt.Println("auto-vpn v1.0.0")
+		os.Exit(0)
+	}
+
+	if *cleanPtr {
+		entries := Unwrap(os.ReadDir(*pathPtr))
+		for _, entry := range entries {
+			if strings.Contains(entry.Name(), ".ovpn") {
+				Expect(os.Remove(*pathPtr + "/" + entry.Name()))
+			}
+		}
+	}
+
+	fmt.Printf("\r- Listening on path \x1b[32m%s\x1b[0m for .ovpn file",*pathPtr)
+	for {
+		select {
+		case <- sigChan:
+			fmt.Println()
+			os.Exit(0)		
+		default:
+			entries := Unwrap(os.ReadDir(*pathPtr))
+			for _, entry := range entries {
+				if strings.Contains(entry.Name(), ".ovpn") {
+					fmt.Println()
+					sudoExe := Unwrap(exec.LookPath("sudo"))
+					syscall.Exec(sudoExe, []string{"sudo", "openvpn", *pathPtr + "/" + entry.Name()}, os.Environ())
+				}
+			}
+			time.Sleep(time.Millisecond*200)
+			fmt.Printf("\r\\ Listening on path \x1b[32m%s\x1b[0m for .ovpn file",*pathPtr)
+			time.Sleep(time.Millisecond*200)
+			fmt.Printf("\r| Listening on path \x1b[32m%s\x1b[0m for .ovpn file",*pathPtr)
+			time.Sleep(time.Millisecond*200)
+			fmt.Printf("\r/ Listening on path \x1b[32m%s\x1b[0m for .ovpn file",*pathPtr)
+			time.Sleep(time.Millisecond*200)
+			fmt.Printf("\r- Listening on path \x1b[32m%s\x1b[0m for .ovpn file",*pathPtr)
+		}
+	}
+}
+
+func Expect(err error) {
+	_, file, line, ok := runtime.Caller(1)
+	progName := "auto-vpn"
+	if !ok { 
+		if err != nil {
+			fmt.Fprintf(
+				os.Stderr, 
+				"\x1b[2m%s\x1b[0m %v\n",
+				progName,
+				err,
+			)
+			os.Exit(1)
+		}
+	} else {
+		if err != nil {
+			fmt.Fprintf(
+				os.Stderr, 
+				"\x1b[2m%s\x1b[0m %v\n",
+				fmt.Sprintf("%s(%s:%d): ",progName, file,line),
+				err,
+			)
+			os.Exit(1)
+		}
+	}
+}
+
+func Unwrap[T any](result T, err error) T {
+	_, file, line, ok := runtime.Caller(1)
+	progName := "auto-vpn"
+	if !ok {
+		if err != nil {
+			fmt.Fprintf(
+				os.Stderr, 
+				"\x1b[2m%s\x1b[0m %v\n",
+				progName,
+				err,
+			)
+			os.Exit(1)
+		}
+	} else {
+		if err != nil {
+			fmt.Fprintf(
+				os.Stderr, 
+				"\x1b[2m%s\x1b[0m %v\n",
+				fmt.Sprintf("%s(%s:%d): ",progName,file,line),
+				err,
+			)
+			os.Exit(1)
+		}
+	}
+	return result
 }
